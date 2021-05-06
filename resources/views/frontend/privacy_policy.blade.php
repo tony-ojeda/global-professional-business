dad60a4064b1654fb609bedeff68a0a1f8bde0ee
Gabriel Sosa <gabriel@codea.pe>
2021-05-06 17:08:39 -0500

    Cambio a inglés

---
+++ resources/views/frontend/privacy_policy.blade.php
@@ -0,0 +1,41 @@
+@extends('frontend.templates.app')
+@section('title', 'Global Professional Business')
+
+@section('content')
+	<div id="main-banner">
+		<div class="item" style="background-image:url('frontend/img/bg-banner.jpg');">
+			<div class="container">
+				<div class="row">
+					<h1>Privacy Policy</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section class="page-text">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <p>El objetivo de Global Professional Business “GPB” es conectar a todos sus miembros de pequeñas, medianas y grandes negocios en el mundo para que puedan ser más productivos y exitosos. Para este directorio de negocios es fundamental y nuestro compromiso de ser transparentes sobre los datos que recopilamos sobre usted y de su empresa, cómo se utilizan y con quién se comparten.</p>
+                    <p>Esta Política de privacidad se aplica cuando utiliza nuestros Servicios (descritos a continuación). Ofrecemos a nuestros usuarios opciones sobre los datos que recopilamos, usamos y compartimos como se describe en esta Política de privacidad, Política de cookies.</p>
+
+                    <h3>Información de "GPB"</h3>
+
+                    <h4>Generalmente</h4>
+                    <p>Somos un directorio en línea de negocios globales para pequeñas y medianas empresas. La membresía utiliza nuestros Servicios para encontrar y ser encontrado para oportunidades comerciales, para ser conectada y ser productiva y exitoso con otros negocios y encontrar información, para su crecimiento global. Nuestra política de privacidad se aplica a cualquier miembro o visitante de nuestros servicios.</p>
+                    <p>Nuestros usuarios registrados y Miembros pueden compartir sus identidades profesionales de negocios, se relacionan con su red global, intercambian conocimientos y perspectivas negocios o profesión, lo que publicas y ven tu contenido relevante, aprenden y desarrollan negocios y encuentran negocios y oportunidades comerciales. El contenido y los datos son visibles para nuestros miembros de negocios comerciales. Y ("Visitantes").</p>
+                    <p>Utilizamos el término "países designados" para referirnos a países de la Unión Europea (UE), el Espacio Económico Europeo (EEE) y Suiza.</p>
+
+                    <h4>Servicios</h4>
+                    <p>Esta Política de privacidad, en la que se incluye nuestra Política de cookies, se aplica al uso que realices de nuestros Servicios.</p>
+                    <p>Nuestra Política de privacidad se aplica a globalprofessionalbusiness.com, a las aplicaciones de la marca GPB, a GPB aprendizaje y a otros sitios relacionados con Global Professional Business, a las aplicaciones, comunicaciones y servicios («Servicios»), incluidos los Servicios ofrecidos fuera del sitio web, como nuestros servicios de publicidad y los complementos (plugins) «Solicitar con GPB» y «Compartir en GPB», pero quedan excluidos los servicios que se ofrecen en virtud de una política de privacidad diferente. Para los residentes en California, nuestro Comunicado de privacidad para California ofrece información adicional en lo que concierne a las obligaciones impuestas por la legislación de California. La Ley de Privacidad del Consumidor de California] ("Información Personal de California"). Para dichos residentes, las disposiciones de este Aviso de Privacidad de California prevalecen sobre cualquier disposición conflictiva de la Política de Privacidad de Global Professional Business.</p>
+
+                    <h4>Responsabilidad de los datos</h4>
+                    <p>Si eres miembro o visitante de otro país será la entidad responsable de sus datos personales que facilites a nuestros servicios, o que se recopilen o procesan por o para nuestros Servicios o en relación con ellos, cada país será responsables de datos que se recopilen o procesen por otros terceros servicios o en relación con ellos.</p>
+                    <p>Si eres Visitante o Miembro de nuestros Servicios, cualesquiera recopilación, uso e información compartida en relación con tus datos personales quedarán sujetos a esta Política de privacidad y otros documentos mencionados en la misma, asícomo sus actualizaciones.</p>
+
+                    <h4>Cómo se usa la información</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
