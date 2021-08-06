0b4623d733f58c4f0ad93de5ddbfaa248bb03431
Gabriel Sosa <gabriel@codea.pe>
2021-08-05 19:08:23 -0500

    Cambio de foto perfil por primer banner en Home

--- resources/views/frontend/templates/app.blade.php
+++ resources/views/frontend/templates/app.blade.php
@@ -82,7 +82,7 @@
                                 <div class="login-nav">
                                     <a href="{{ route('frontend.directory.register') }}" class="btn btn-outline-white">New Business</a>
                                     <a href="{{ route('frontend.directory.my_business') }}" class="btn btn-red">{{ Auth::user()->name }}</a>
-                                    <a href="{{ route('frontend.logout') }}" class="btn-logout">
+                                    <a href="{{ route('frontend.logout') }}" class="btn btn-logout">
                                         {{-- <img src="{{ asset('frontend/img/logout.svg') }}" alt=""> --}}
                                         Logout
                                     </a>
